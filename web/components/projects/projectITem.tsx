11a0c2c0847ac3a23c741d77f4411844a147351d
toandq <dqtoan0123@gmail.com>
2024-03-29 17:10:20 +0700

    met vcl

--- web/components/projects/projectITem.tsx
+++ web/components/projects/projectITem.tsx
@@ -28,7 +28,7 @@ const ProjectITem: React.FC<IPropsProjectItem> = ({ dataItem }) => {
     const noti = useNoti()
     const info = useSelector(selectInfo);
     
-    console.log('test cicd sssdf')
+    console.log('test cicd sssdssaaaaaf')
 
     return (
         <div className="rounded cursor-pointer bg-theme-secondary border shadow-theme-secondary">
