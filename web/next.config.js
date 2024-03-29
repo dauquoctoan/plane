a4c3ea35efc0ecb60062c5b5f2da1a2d4414852b
toandq <dqtoan0123@gmail.com>
2024-03-29 22:20:07 +0700

    [web] - fix image

--- web/next.config.js
+++ web/next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
     mode: 'production',
     images: {
-        // remotePatterns: [
-        //   {
-        //     protocol: 'https',
-        //     hostname: 'planefs.s3.us-east-1.amazonaws.com',
-        //     port: '',
-        //     pathname: '/static/project-cover/**',
-        //   },
-        // ],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'planefs.s3.us-east-1.amazonaws.com',
+            port: '443',
+            pathname: '/static/project-cover/**',
+          },
+        ],
     },
 };
 
