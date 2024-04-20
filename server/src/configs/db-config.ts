d99356bd1eb40e15be0be3edb142855e1612eb7f
toandq <dqtoan0123@gmail.com>
2024-04-20 16:04:17 +0700

    [ci/cd] setup workflows

--- server/src/configs/db-config.ts
+++ server/src/configs/db-config.ts
@@ -5,7 +5,7 @@ import { Dialect } from "sequelize";
 export const CONFIG_DB: SequelizeModuleAsyncOptions = {
   useFactory: async (configService: ConfigService) => ({
     dialect: configService.get<Dialect>('DATABASE') || 'mysql',
-    host: 'localhost',
+    host: configService.get<string>('DB_HOST') || 'localhost',
     port: configService.get<number>('DB_PORT') || 3006,
     username: configService.get<string>('DB_USER_NAME') || 'root',
     password: configService.get<string>('DB_PASSWORD') || '123456',
