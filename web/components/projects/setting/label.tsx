45a198fd2b1c56a5f77746525b696c2acdcb72ef
Phú Trọng <phamducphutrong@gmail.com>
2024-06-20 23:03:40 +0700

    add create label button

--- web/components/projects/setting/label.tsx
+++ web/components/projects/setting/label.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { LABELS_BY_PROJECT_KEY } from "@/apiKey";
+import CreateLabel from "@/components/issue/createLabel";
+import Button from "@/components/ui/button";
 import Input from "@/components/ui/input/Input";
+import Modal from "@/components/ui/modal";
 import { icons } from "@/constants";
 import { useNoti } from "@/hooks";
 import issueService from "@/services/issue-services";
@@ -10,48 +13,58 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LabelSetting = ()=>{
+const LabelSetting = () => {
     const params = useParams<IParams>();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const {data} = useSWR(LABELS_BY_PROJECT_KEY(params.projectid),()=>{
-        return issueService.findLabelsByProject({projectId: params.projectid})
+    const { data: labels } = useSWR(LABELS_BY_PROJECT_KEY(params.projectid), () => {
+        return issueService.findLabelsByProject({ projectId: params.projectid })
     })
 
     return <div className="flex flex-col gap-2">
-        {
-            data?.map((e,i)=>{
-                return <LabelSettingItem key={i} data={e}/>
-            })
-        }
+        <Button typeBTN="primary" className="mb-2" text="Create Label" onClick={() => { setIsOpen(true) }} />
+        {labels?.map((e, i) => <LabelSettingItem key={i} data={e} />)}
+        <Modal
+            isOpen={isOpen}
+            handleClose={() => {
+                setIsOpen(false)
+            }}
+            content={
+                <CreateLabel
+                    handleClose={() => { setIsOpen(false) }}
+                    projectId={params.projectid}
+                />
+            }
+        />
     </div>
 }
 
-const LabelSettingItem = ({data}:{data: ILabel})=>{
+const LabelSettingItem = ({ data }: { data: ILabel }) => {
     const [isEddit, setIsEdit] = useState(false);
     const noti = useNoti();
 
     return <div className="border h-11 rounded-md px-2 py-1 flex items-center gap-2 w-full justify-between">
-            <div className="flex items-center gap-2 justify-center">
-            <div style={{background:data.color}} className="w-3 h-3 rounded-full"></div>
+        <div className="flex items-center gap-2 justify-center">
+            <div style={{ background: data.color }} className="w-3 h-3 rounded-full"></div>
             {
-                !isEddit ? <div>{data.name}</div>:<Input autoFocus style={{border:'none'}} onKeyDown={async (e:any)=>{
+                !isEddit ? <div>{data.name}</div> : <Input autoFocus style={{ border: 'none' }} onKeyDown={async (e: any) => {
                     if (e.keyCode == 13) {
-                        const result = await issueService.updateLabel({name: e?.target?.value as any}, data.id)
-                        if(result){
+                        const result = await issueService.updateLabel({ name: e?.target?.value as any }, data.id)
+                        if (result) {
                             setIsEdit(false);
                             noti?.success('Update label success')
                         }
-                        else  noti?.error('Update label error')
+                        else noti?.error('Update label error')
                     }
-                }} defaultValue={data.name}/>
+                }} defaultValue={data.name} />
             }
-            </div>
-            <div onClick={()=>{
-                setIsEdit(!isEddit)
-            }}>
-                {icons.edit}
-            </div>
-</div>
+        </div>
+        <div onClick={() => {
+            setIsEdit(!isEddit)
+        }}>
+            {icons.edit}
+        </div>
+    </div>
 }
 
 export default LabelSetting;
--- web/components/projects/setting/state.tsx
+++ web/components/projects/setting/state.tsx
@@ -14,67 +14,64 @@ import useSWR, { mutate } from 'swr'
 
 const StateSetting = () => {
     const params = useParams<IParams>()
-    const{data} = useSWR(STATES_KEY(params.projectid),()=>{
-        return issueService.getState(params.projectid)
-    })
 
     const noti = useNoti();
 
-    const [isOpen, setIsOpen]  = useState<boolean>(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const { data: states } = useSWR(
         STATES_KEY(params.projectid),
         () => issueService.getState(params.projectid),
     );
 
-  return (
-    <div>
-        <Button typeBTN='primary' className='mb-2' text='Create State' onClick={()=>{
-            setIsOpen(true);
-        }}/>
-        <div className='flex flex-col gap-2'>
-        {
-            data?.map((e,i)=>{
-                return <div key={i} className='px-2 py-1 justify-between border rounded flex gap-2 items-center'>
-                    <div className='flex items-center gap-2'>
-                        <div>{getIcons(e.group, e.color)}</div>
-                        <div className='text-sm'>{e.name}</div>
-                    </div>
-                    <div onClick={async ()=>{
-                        if(e.project_id){
-                            const result = await issueService.deleteState(e.id)
-                            if(result){
-                                noti?.success('Delete state success');
-                                mutate(STATES_KEY(params.projectid), (data:any)=>{
-                                    return data.filter((item:Istate)=>{
-                                        return item.id != e.id;
-                                    })
-                                })
-                            }else noti?.error('Delete state error')
+    return (
+        <div>
+            <Button typeBTN='primary' className='mb-2' text='Create State' onClick={() => {
+                setIsOpen(true);
+            }} />
+            <div className='flex flex-col gap-2'>
+                {
+                    states?.map((e, i) => {
+                        return <div key={i} className='px-2 py-1 justify-between border rounded flex gap-2 items-center'>
+                            <div className='flex items-center gap-2'>
+                                <div>{getIcons(e.group, e.color)}</div>
+                                <div className='text-sm'>{e.name}</div>
+                            </div>
+                            <div onClick={async () => {
+                                if (e.project_id) {
+                                    const result = await issueService.deleteState(e.id)
+                                    if (result) {
+                                        noti?.success('Delete state success');
+                                        mutate(STATES_KEY(params.projectid), (data: any) => {
+                                            return data.filter((item: Istate) => {
+                                                return item.id != e.id;
+                                            })
+                                        })
+                                    } else noti?.error('Delete state error')
+                                }
+                            }}>{icons.delete}</div>
+                        </div>
+                    })
+                }
+                {
+                    states && <Modal
+                        isOpen={isOpen}
+                        handleClose={() => {
+                            setIsOpen(false)
+                        }}
+                        content={
+                            <CreateState
+                                handleClose={() => {
+                                    setIsOpen(false);
+                                }}
+                                projectId={params.projectid}
+                                states={states}
+                            />
                         }
-                    }}>{icons.delete}</div>
-                </div>
-            })
-        }
-        {
-           states &&  <Modal
-            isOpen={isOpen}
-            handleClose={() => {
-                setIsOpen(false)
-            }}
-            content={
-                <CreateState
-                    handleClose={() => {
-                        setIsOpen(false);
-                    }}
-                    projectId={params.projectid}
-                    states={states}
-                />
-            }
-        />
-        }
+                    />
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default StateSetting;
